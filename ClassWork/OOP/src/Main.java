0c2fe4014ecf60a235516a8074cfcbf3ff350472
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-11-27 00:42:53 +0700

    add file

---
+++ ClassWork/OOP/src/Main.java
@@ -0,0 +1,9 @@
+import model.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Person person = new Person();
+
+    }
+}
