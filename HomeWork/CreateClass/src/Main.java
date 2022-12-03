56f82db506df1a6c58faa22ad232a1fc3cbb5191
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-12-04 02:54:23 +0700

    add

--- HomeWork/CreateClass/src/Main.java
+++ HomeWork/CreateClass/src/Main.java
@@ -1,5 +1,29 @@
+import com.example.oop.Fraction;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Математические операции с дробями с переводом в правильную и в неправильную дробь и с сокращением:");
+
+        Fraction A = new Fraction(3, 2);
+        Fraction B = new Fraction(2, 5);
+
+        Fraction A1 = new Fraction(4,1, 4);
+        Fraction B1 = new Fraction(3,2, 5);
+
+        System.out.println("Сложение:");
+        Fraction addition = Fraction.addition(A, B);
+        Fraction addition1 = Fraction.addition(A1, B1);
+
+        System.out.println("Вычитание:");
+        Fraction subtraction = Fraction.subtraction(A, B);
+        Fraction subtraction1 = Fraction.subtraction(A1, B1);
+
+        System.out.println("Умножение:");
+        Fraction multiply = Fraction.multiply(A, B);
+        Fraction multiply1 = Fraction.multiply(A1, B1);
+
+        System.out.println("Деление:");
+        Fraction division = Fraction.division(A, B);
+        Fraction division1 = Fraction.division(A1, B1);
     }
 }
--- HomeWork/CreateClass/src/com/example/oop/Fraction.java
+++ HomeWork/CreateClass/src/com/example/oop/Fraction.java
@@ -5,9 +5,122 @@ public class Fraction {
     public int numerator;
     public int denominator;
 
+    void setDenominator(int denominator)
+    {
+        if (denominator == 0)throw new IllegalArgumentException("The denominator is zero.");
+        this.denominator = denominator;
+    }
+
+    void setNumerator(int numerator) {
+        if (numerator >= 0)this.numerator = numerator;
+		else this.numerator = -numerator;
+    }
+
     public Fraction() {
         integer = 0;
         numerator = 0;
         denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator) {
+        this.setNumerator(numerator);
+        this.setDenominator(denominator);
+    }
+
+    public Fraction(int integer, int numerator, int denominator) {
+        this.integer = integer;
+        this.setNumerator(numerator);
+        this.setDenominator(denominator);
+    }
+
+    void to_proper()
+    {
+        int i = 1;
+        for (; this.numerator >= this.denominator; i++)
+        {
+            this.numerator = this.numerator - this.denominator;
+            this.integer = i;
+        }
+    }
+    void to_improper()
+    {
+        this.numerator = this.integer * this.denominator + this.numerator;
+        this.integer = 0;
+    }
+    void reduce()
+    {
+        int CommonMultiplier;
+        do {
+            if ((this.numerator <= this.denominator) && (this.numerator != 0))
+                CommonMultiplier = this.numerator;
+			else if (this.numerator > this.denominator)
+                CommonMultiplier = this.denominator;
+			else break;
+            for (; CommonMultiplier > 0; CommonMultiplier--)
+            {
+                if (this.numerator % CommonMultiplier == 0 && this.denominator % CommonMultiplier==0)
+                {
+                    this.numerator = this.numerator / CommonMultiplier;
+                    this.denominator = this.denominator / CommonMultiplier;
+                    break;
+                }
+            }
+        } while (CommonMultiplier != 1);
+        this.to_proper();
+    }
+
+    public static Fraction addition(Fraction left, Fraction right)
+    {
+        left.to_improper();
+        right.to_improper();
+        Fraction result = new Fraction(left.numerator*right.denominator + right.numerator*left.denominator, left.denominator*right.denominator);
+        result.reduce();
+        result.print();
+        return result;
+    }
+
+    public static Fraction subtraction(Fraction left, Fraction right)
+    {
+        left.to_improper();
+        right.to_improper();
+        Fraction result = new Fraction(left.numerator*right.denominator - right.numerator*left.denominator, left.denominator*right.denominator);
+        result.reduce();
+        result.print();
+        return result;
+    }
+
+    public static Fraction multiply(Fraction left, Fraction right)
+    {
+        left.to_improper();
+        right.to_improper();
+        Fraction result = new Fraction(left.numerator*right.numerator,left.denominator*right.denominator);
+        result.reduce();
+        result.print();
+        return result;
+    }
+
+    public static Fraction division(Fraction left, Fraction right)
+    {
+        left.to_improper();
+        right.to_improper();
+        Fraction result = new Fraction(left.numerator*right.denominator,left.denominator*right.numerator);
+        result.reduce();
+        result.print();
+        return result;
+    }
+
+    void print()
+    {
+
+        if (this.numerator!=0 && this.integer!=0)
+        {
+            System.out.println (integer + "(" + numerator + "/" + denominator + ")" );
+        }
+		else if (this.integer==0 && this.numerator!=0)
+        {
+            System.out.println (numerator + "/" + denominator);
+        }
+		else
+            System.out.println(integer);
     }
 }
