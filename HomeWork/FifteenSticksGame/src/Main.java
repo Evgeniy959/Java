f5c73ab840ccf82f36fa14a94e8279fa9c3cee2c
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-12-16 01:40:22 +0700

    add

--- HomeWork/FifteenSticksGame/src/Main.java
+++ HomeWork/FifteenSticksGame/src/Main.java
@@ -1,20 +1,61 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static String sticks = "";
-    public static int stick = 15;
+    public static int sticksCount = 15;
 
-    public static void startGame(){
+    public static void Game(int stick){
         for (int i = 0; i < stick; i++)
         {
             sticks = "|";
             System.out.print(sticks);
-            /*label1.Text = (sticks);
-            label2.Text = (stick.ToString());*/
         }
+        System.out.println();
+    }
+
+    public static int random(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        startGame();
+        Scanner in = new Scanner(System.in);
+        /*System.out.println("Start");
+        Game(sticksCount);
+        while (sticksCount>0){
+            if(sticksCount==1){
+                System.out.println("Gamer1 Вы проиграли!"); break;}
+            else System.out.println("Gamer1");
+            int a = in.nextInt();;
+            sticksCount -=a;
+            Game(sticksCount);
+            if(sticksCount==1){
+                System.out.println("Gamer2 Вы проиграли!"); break;}
+            else System.out.println("Gamer2");
+            int b = in.nextInt();;
+            sticksCount -=b;
+            Game(sticksCount);
+        }*/
+
+// artificial intelligence
+
+        System.out.println("Start");
+        Game(sticksCount);
+        while (sticksCount>0){
+            if(sticksCount==1){
+                System.out.println("Вы проиграли!"); break;}
+            else System.out.println("Gamer1");
+            int a = in.nextInt();;
+            sticksCount -=a;
+            Game(sticksCount);
+            if(sticksCount==1){
+                System.out.println("Вы выйграли!"); break;}
+            else System.out.println("PC");
+            int b = random(1, 3);
+            sticksCount -=b;
+            Game(sticksCount);
+        }
 
     }
 }
