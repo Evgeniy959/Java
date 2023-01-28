0b5a3dda3704fd7dea9ea5f1d0a7ac9b338ad59d
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2023-01-28 21:07:30 +0700

    Add

---
+++ ClassWork/MyJavaSpring/src/main/java/com/topacademy/springapp/ClassMusic.java
@@ -0,0 +1,7 @@
+package com.topacademy.springapp;
+
+public class ClassMusic implements Music{
+    public String getSong() {
+        return "This is a classic music";
+    }
+}
---
+++ ClassWork/MyJavaSpring/src/main/java/com/topacademy/springapp/Main.java
@@ -0,0 +1,29 @@
+package com.topacademy.springapp;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Okey");
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applocationContext.xml");
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+
+        System.out.println(musicPlayer.playMusic());
+        System.out.println(musicPlayer.playMusicList());
+
+ //       TestBean testBean = context.getBean("testBean", TestBean.class);
+        //       // TestBean testBean2 = context.getBean("testBean2",TestBean.class);
+//
+//        System.out.println(  TestBean.factoryMethod().toString());
+//   //     testBean2.setName("Ivan");
+//
+//        System.out.println(testBean);
+//     //   System.out.println(testBean2);
+        context.close();
+//        System.out.println(testBean.toString());
+//        System.out.println(testBean); // Выводит то же, что и при вызове метода toString()
+
+    }
+}
