65d2fb2948dc17eb355637a651c8f5e358b1bf35
Pili_zr36 <77540303+Evgeniy959@users.noreply.github.com>
2023-03-07 01:01:59 +0700

    add

---
+++ HomeWork/RoomBooking-Layout/RoomBooking — new/src/main/java/com/topacademy/roombooking/controller/RoomController.java
@@ -0,0 +1,35 @@
+package com.topacademy.roombooking.controller;
+
+import com.topacademy.roombooking.model.Room;
+import com.topacademy.roombooking.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class RoomController {
+    private final RoomService roomService;
+    @Autowired
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping("/room")
+    public String search(Model model){
+        Room room = new Room();
+        model.addAttribute("room", room);
+        return "layouts/search";
+    }
+
+    @PostMapping("/search")
+    public String findByDate(@ModelAttribute Room room, Model model){
+        List<Room> rooms = roomService.findByDate(room.getEnd(), room.getStart());
+        model.addAttribute("rooms", rooms);
+        return "layouts/room-list";
+    }
+}
